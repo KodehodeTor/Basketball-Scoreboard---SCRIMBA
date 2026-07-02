5c3fe4668079714425bd8b5b3d4b9efa6fbc00f2
KodehodeTor <torjarand.26pg@gmail.com>
2026-07-02 10:14:07 +0200

    Function for buttons and display set up

--- script.js
+++ script.js
@@ -1,18 +1,44 @@
 // Functions for increments
 
-let count = 0;
+let count1 = 0;
+let count2 = 0;
+let count11 = document.getElementById("count1");
+let count22 = document.getElementById("count2");
 
-function inc1() {
-  count += 1;
-  document.getElementById("inc1-btn").textContent;
+// let inc1btn = document.getElementById("inc1-btn");
+// let inc2btn = document.getElementById("inc2-btn");
+// let inc3btn = document.getElementById("inc3-btn");
+
+// HOME
+
+function inc11() {
+  count1 += 1;
+  count11.textContent = count1;
 }
 
-function inc2() {
-  count += 2;
-  document.getElementById("inc2-btn").textContent;
+function inc21() {
+  count1 += 2;
+  count11.textContent = count1;
 }
 
-function inc3() {
-  count += 3;
-  document.getElementById("inc3-btn").textContent;
+function inc31() {
+  count1 += 3;
+  count11.textContent = count1;
+}
+
+// GUESTS
+
+function inc12() {
+  count2 += 1;
+  count22.textContent = count2;
+}
+
+function inc22() {
+  count2 += 2;
+  count22.textContent = count2;
+}
+
+function inc32() {
+  count2 += 3;
+  count22.textContent = count2;
 }
